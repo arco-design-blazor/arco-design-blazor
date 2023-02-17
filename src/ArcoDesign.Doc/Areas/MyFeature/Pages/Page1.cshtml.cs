022c13c8982583b99eee243dae5cac26eed73cb2
sealoyal <sealoyal@hotmail.com>
2023-02-17 14:02:37 +0800

    新增：框架项目

---
+++ src/ArcoDesign.Doc/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ArcoDesign.Doc.MyFeature.Pages;
+public class Page1Model : PageModel {
+    public void OnGet() {
+
+    }
+}
