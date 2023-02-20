f5035a73014b62a48c6087c7589356e28fb93e8d
sealoyal <sealoyal@hotmail.com>
2023-02-20 09:28:45 +0800

    修改: ArcoDesign 项目类型

---
+++ src/ArcoDesign1/Infra/CssBuilders/AttributeBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcoDesign.Infra.CssBuilders;
+
+/// <summary>
+/// attribute 构建器
+/// </summary>
+public class AttributeBuilder: KeyValueBuilder<string, object> {
+
+}
