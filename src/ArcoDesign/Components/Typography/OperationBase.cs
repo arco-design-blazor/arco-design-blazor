63c942371a56608226ff5ae8651eb7438a463bd3
sealoyal <sealoyal@hotmail.com>
2023-03-01 10:51:08 +0800

    草稿

---
+++ src/ArcoDesign/Components/Typography/OperationBase.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace ArcoDesign.Components.Typography;
+public abstract class OperationBase: ArcoDesignComponentBase {
+    protected readonly Dictionary<string, object> currentContext = new Dictionary<string, object>();
+
+    [Parameter]
+    public bool Copyable { get; set; }
+
+    [Parameter]
+    public RenderFragment CopyableNode { get; set; }
+    [Parameter]
+    public RenderFragment EditableNode { get; set; }
+    [Parameter]
+    public bool Editable { get; set; }
+    [Parameter]
+    public bool Ellipsis { get; set; }
+    [Parameter]
+    public bool IsEllipsis { get; set; }
+    [Parameter]
+    public bool Expanding { get; set; }
+    [Parameter]
+    public bool ForceShowExpand { get; set; }
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClickExpand { get; set; }
+    [Parameter]
+    public EventCallback<bool> SetEditing { get; set; }
+}
+public class CopyableNode : ArcoDesignComponentBase {
+    [Parameter]
+    public string Text { get; set; }
+
+    [Parameter]
+    public EventCallback<string> OnCopy { get; set; }
+
+    [Parameter]
+    public RenderFragment IconNode { get; set; }
+    [Parameter]
+    public RenderFragment LeftToolTip { get; set; }
+    [Parameter]
+    public RenderFragment RightToolTip { get; set; }
+}
+
+public class EditableNode : ArcoDesignComponentBase {
+    public bool Editing { get; set; }
+    public EventCallback<string> OnStart { get; set; }
+    public EventCallback<string> OnChange { get; set; }
+    public EventCallback<string> OnEnd { get; set; }
+}
---
+++ src/ArcoDesign/Components/Typography/TypographyBase.cs
@@ -0,0 +1,62 @@
+﻿using ArcoDesign.Components.Typography;
+using Microsoft.AspNetCore.Components;
+
+namespace ArcoDesign.Components;
+
+public abstract class TypographyBase : OperationBase {
+    /// <summary>
+    /// 内容项
+    /// </summary>
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+
+    /// <summary>
+    /// 文本类型
+    /// </summary>
+    [Parameter]
+    public TextType Type { get; set; }
+
+    /// <summary>
+    /// 加粗
+    /// </summary>
+    [Parameter]
+    public bool Bold { get; set; } = false;
+
+    /// <summary>
+    /// 禁用
+    /// </summary>
+    [Parameter]
+    public bool Disabled { get; set; } = false;
+
+    /// <summary>
+    /// 标记
+    /// </summary>
+    [Parameter]
+    public bool Mark { get; set; }
+
+    /// <summary>
+    /// 下划线
+    /// </summary>
+    [Parameter]
+    public bool Underline { get; set; } = false;
+
+    /// <summary>
+    /// 删除线
+    /// </summary>
+    [Parameter]
+    public bool Delete { get; set; } = false;
+
+    /// <summary>
+    /// 代码块
+    /// </summary>
+    [Parameter] 
+    public bool Code { get; set; } = false; 
+}
+
+public enum TextType {
+    Primary,
+    Secondary,
+    Success,
+    Error,
+    Warning
+}
